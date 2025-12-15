9c4784e0e122a94959c6c0e2487f94c77a950342
Robabobaboss <113091589+robaboss@users.noreply.github.com>
2025-12-15 12:37:28 +0100

    Add files via upload

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, HttpClientModule],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'] 
+})
+export class RegisterComponent {
+  name: string = "";
+  email: string = "";
+  username: string = "";
+  password: string = "";
+
+  constructor(private http: HttpClient) {}
+
+  register() {
+    const regisztracio = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    };
+
+     this.http.post('http://localhost:3000/register', regisztracio) 
+      .subscribe(res => {
+        console.log("Sikeres regisztráció!");
+        this.name = '';
+        this.email = '';
+        this.username = '';
+        this.password = '';
+      }, error => {
+        console.error("Hiba a regisztráció során!", error);
+      });
+  }
+}
