4ddf9b55d1a690d905bcc5e8178cf032b3c6a96d
Cs4byk4hh <101185780+Cs4byk4hh@users.noreply.github.com>
2026-01-14 08:02:21 +0100

    Update index.js

--- Vizsgaremek_Kosaradd/models/index.js
+++ Vizsgaremek_Kosaradd/models/index.js
@@ -2,10 +2,12 @@ const sequelize = require('../config/db');
 const felhasznalok = require('./felhasznalok');
 const velemenyek = require('./velemenyek');
 const perfumes = require('./perfumes');
+const kosar = require('./kosar');
 
 module.exports = {
     sequelize,
     felhasznalok,
     velemenyek,
     perfumes
+
 };
