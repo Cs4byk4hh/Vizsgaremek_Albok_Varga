4b42f72d3633e02c932011819102e188bf5e1a12
Cs4byk4hh <101185780+Cs4byk4hh@users.noreply.github.com>
2026-02-03 11:32:22 +0100

    Update index.js

--- Vizsgaremek_Kosaradd/models/index.js
+++ Vizsgaremek_Kosaradd/models/index.js
@@ -5,6 +5,10 @@ const perfumes = require('./perfumes');
 const kosar = require('./kosar');
 const kosar_tartalom = require('./kosar_tartalom);
 
+kosar_tartalom.belongsTo(kosar, { foreignKey: 'kosar_id' });
+kosar_tartalom.belongsTo(perfumes, { foreignKey: 'parfum_id' });
+velemenyek.belongsTo(felhasznalok, { foreignKey: 'user_id' });
+
 module.exports = {
     sequelize,
     felhasznalok,
@@ -13,6 +17,7 @@ module.exports = {
     kosar,
     kosar_tartalom
 };
+
 
 
 
