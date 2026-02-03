a4e89af4ee0f8dc97ebe76475e97198253c8920d
Cs4byk4hh <101185780+Cs4byk4hh@users.noreply.github.com>
2026-02-03 11:30:45 +0100

    Update index.js

--- Vizsgaremek_Admin/models/index.js
+++ Vizsgaremek_Admin/models/index.js
@@ -5,6 +5,7 @@ const perfumes = require('./perfumes');
 const kosar_tartalom = require('./kosar_tartalom');
 const kosar = require('./kosar');
 
+kosar_tartalom.belongsTo(kosar, { foreignKey: 'kosar_id' });
 kosar_tartalom.belongsTo(perfumes, { foreignKey: 'parfum_id' });
 velemenyek.belongsTo(felhasznalok, { foreignKey: 'user_id' });
 
@@ -18,4 +19,5 @@ module.exports = {
     kosar
 
 };
+
 
